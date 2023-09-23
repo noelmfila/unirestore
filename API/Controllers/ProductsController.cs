00ee637ddaa10cce1bdbcc051da107645d11e3fe
Noel Tshuma <noelmfila@gmail.com>
2023-09-23 02:25:01 +0200

    add actionresult to controller methods

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -22,13 +22,13 @@ namespace API.Controllers
         }
 
         [HttpGet] //api/products
-        public async Task<List<Product>> GetProductsAsync()
+        public async Task<ActionResult<List<Product>>> GetProductsAsync()
         {
             return await _context.Products.ToListAsync();
         }
 
         [HttpGet("{id}")] //api/products/2 
-        public async Task<Product> GetProductAsync(int id)
+        public async Task<ActionResult<Product>> GetProductAsync(int id)
         {
             return await _context.Products.FindAsync(id);
         }
