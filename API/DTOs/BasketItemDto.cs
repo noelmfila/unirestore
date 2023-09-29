bface6b27df36feba62cf885549be4686e67140f
Noel Tshuma <noelmfila@gmail.com>
2023-09-30 01:06:03 +0200

    add basket functionality

--- API/DTOs/BasketItemDto.cs
+++ API/DTOs/BasketItemDto.cs
@@ -1,14 +1,14 @@
-namespace API.Entities
+namespace API.DTOs
 {
-    public class Product
+    public class BasketItemDto
     {
-        public int Id { get; set; }
+        public int ProductId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public long Price { get; set; }
         public string PictureUrl { get; set; }
         public string Type { get; set; }
         public string Brand { get; set; }
-        public int QuantityInStock { get; set; }
+        public int Quantity { get; set; }
     }
 }
